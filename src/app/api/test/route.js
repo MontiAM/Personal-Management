4f479050e81620ad28e826956b582cbc9dcd6eef
Marcio Montivero <montivero.marcio@gmail.com>
2024-08-19 17:01:18 -0300

    daily expenses

---
+++ src/app/api/test/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  return NextResponse.json(
+    {
+      message: "Anda",
+    },
+    {
+      status: 200,
+    }
+  );
+}
