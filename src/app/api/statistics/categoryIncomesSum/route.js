e60b49641361477e11addcd2aa08bab5c88fe2e4
Marcio Montivero <montivero.marcio@gmail.com>
2024-09-04 13:02:44 -0300

    Listo API categoryIncomesSum

---
+++ src/app/api/statistics/categoryIncomesSum/route.js
@@ -0,0 +1,88 @@
+import { NextResponse } from "next/server";
+// import { authOptions  } from "../auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+import db from "@/libs/db";
+import getPreviousMonthRange from "@/helpers/getPreviousMonthRange";
+
+export async function GET(request) {
+  // const session = await getServerSession(authOptions);
+
+  // if (!session) {
+  //   return NextResponse.json({
+  //     message: "Unauthorized"
+  //   }, {
+  //     status: 401
+  //   })
+  // }
+
+  // http://localhost:3000/api/statistics/totalExpenses?fecha_desde=2024-08-01&fecha_hasta=2024-08-31
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const fecha_desde = searchParams.get("fecha_desde");
+    const fecha_hasta = searchParams.get("fecha_hasta");
+
+    if (fecha_desde && fecha_hasta) {
+      const startDate = new Date(fecha_desde);
+      const endDate = new Date(fecha_hasta);
+
+      if (isNaN(startDate) || isNaN(endDate)) {
+        return NextResponse.json(
+          { message: "Invalid date format. Use YYYY-MM-DD." },
+          { status: 400 }
+        );
+      }
+
+      const previousMonthRange = getPreviousMonthRange(startDate);
+
+      const totalIncomesCurrent = await db.daily_incomes.groupBy({
+        by: ["income_category"],
+        where: {
+          income_date: {
+            gte: startDate,
+            lte: endDate,
+          },
+        },
+        _sum: {
+          income_amount: true,
+        },
+      });
+
+      const totalIncomesPrevious = await db.daily_incomes.groupBy({
+        by: ["income_category"],
+        where: {
+          income_date: {
+            gte: previousMonthRange.startDate,
+            lte: previousMonthRange.endDate,
+          },
+        },
+        _sum: {
+          income_amount: true,
+        },
+      });
+
+      const transformedCurrentPeriod = totalIncomesCurrent.map((item) => ({
+        income_amount: item._sum.income_amount,
+        income_category: item.income_category,
+      }));
+
+      const transformedPreviousPeriod = totalIncomesPrevious.map((item) => ({
+        income_amount: item._sum.income_amount,
+        income_category: item.income_category,
+      }));
+
+      return NextResponse.json(
+        {
+          currentPeriod: transformedCurrentPeriod,
+          previousPeriod: transformedPreviousPeriod,
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching total incomes: ", error.message);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ message: "GET response" }, { status: 200 });
+}
